16123f36de48b3b0b6151d7123b987644fa4ec91
Sunny <sunnyliu776@gmail.com>
2024-05-02 09:34:43 -0400

    y-sorting done in the main stage and player movement changed to include diagonal movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,30 +7,25 @@ func _ready():
 	$AnimatedSprite2D.play("front_idle")
 
 func _physics_process(delta):
-	player_movement(delta)
+	player_movement()
 	
 
-func player_movement(delta):
+func player_movement():
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up","ui_down")
+	velocity = direction * speed
+	
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
 		play_anim(1)
-		velocity.x = speed
-		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
 		current_dir = "left"
 		play_anim(1)
-		velocity.x = -speed
-		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
 		current_dir = "down"
 		play_anim(1)
-		velocity.x = 0
-		velocity.y = speed
 	elif Input.is_action_pressed("ui_up"):
 		current_dir = "up"
 		play_anim(1)
-		velocity.x = 0
-		velocity.y = -speed
 	else: 
 		play_anim(0)
 		velocity.x = 0
